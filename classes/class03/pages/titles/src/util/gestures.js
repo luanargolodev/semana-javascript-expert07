ecd18085f831c78aa87f316b2424f3b4d4d9e063
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2023-03-01 20:57:16 -0300

    feat: start class 03

---
+++ classes/class03/pages/titles/src/util/gestures.js
@@ -0,0 +1,34 @@
+const { GestureDescription, Finger, FingerCurl } = window.fp
+
+const scrollDownGesture = new GestureDescription('scroll-down') // ✊️
+const scrollUpGesture = new GestureDescription('scroll-up') // 🖐
+
+// Scroll Down
+// -----------------------------------------------------------------------------
+
+// thumb: half curled
+// accept no curl with a bit lower confidence
+scrollDownGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0)
+scrollDownGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.5)
+
+// all other fingers: curled
+for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+  scrollDownGesture.addCurl(finger, FingerCurl.FullCurl, 1.0)
+  scrollDownGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9)
+}
+
+// Scroll Up
+// -----------------------------------------------------------------------------
+
+// no finger should be curled
+for (let finger of Finger.all) {
+  scrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0)
+}
+
+const knowGestures = [scrollDownGesture, scrollUpGesture]
+const gestureStrings = {
+  'scroll-down': '✊️',
+  'scroll-up': '🖐',
+}
+
+export { knowGestures, gestureStrings }
