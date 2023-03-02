b86748dfdc9fecc7db7f8f1652b83d52034a67fb
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2023-03-01 22:29:39 -0300

    feat: detect click finger

--- classes/class03/pages/titles/src/controllers/handGestureController.js
+++ classes/class03/pages/titles/src/controllers/handGestureController.js
@@ -1,6 +1,7 @@
 import { prepareRunChecker } from '../../../../lib/shared/util.js'
 
 const { shouldRun: scrollShouldRun } = prepareRunChecker({ timerDelay: 200 })
+const { shouldRun: clickShouldRun } = prepareRunChecker({ timerDelay: 400 })
 
 export default class HandGestureController {
   #view
@@ -45,6 +46,13 @@ export default class HandGestureController {
       }
 
       for await (const { event, x, y } of this.#service.detectGestures(hands)) {
+        if (event === 'click') {
+          if (!clickShouldRun()) continue
+
+          this.#view.clickOnElement(x, y)
+          continue
+        }
+
         if (event.includes('scroll')) {
           if (!scrollShouldRun()) continue
 
--- classes/class03/pages/titles/src/util/gestures.js
+++ classes/class03/pages/titles/src/util/gestures.js
@@ -2,6 +2,7 @@ const { GestureDescription, Finger, FingerCurl } = window.fp
 
 const scrollDownGesture = new GestureDescription('scroll-down') // ✊️
 const scrollUpGesture = new GestureDescription('scroll-up') // 🖐
+const clickGesture = new GestureDescription('click') // 🤏
 
 // Scroll Down
 // -----------------------------------------------------------------------------
@@ -25,10 +26,32 @@ for (let finger of Finger.all) {
   scrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0)
 }
 
-const knownGestures = [scrollDownGesture, scrollUpGesture]
+// Click
+// -----------------------------------------------------------------------------
+clickGesture.addCurl(Finger.Index, FingerCurl.HalfCurl, 0.8)
+clickGesture.addCurl(Finger.Index, FingerCurl.FullCurl, 0.5)
+
+// dedão
+clickGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
+clickGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.4)
+
+// dedo médio
+clickGesture.addCurl(Finger.Middle, FingerCurl.HalfCurl, 1.0)
+clickGesture.addCurl(Finger.Middle, FingerCurl.FullCurl, 0.9)
+
+// dedo anelar
+clickGesture.addCurl(Finger.Ring, FingerCurl.HalfCurl, 1.0)
+clickGesture.addCurl(Finger.Ring, FingerCurl.FullCurl, 0.9)
+
+// dedo mindinho
+clickGesture.addCurl(Finger.Pinky, FingerCurl.HalfCurl, 1.0)
+clickGesture.addCurl(Finger.Pinky, FingerCurl.FullCurl, 0.9)
+
+const knownGestures = [scrollDownGesture, scrollUpGesture, clickGesture]
 const gestureStrings = {
   'scroll-down': '✊️',
   'scroll-up': '🖐',
+  click: '🤏',
 }
 
-export { knownGestures, gestureStrings }
+export { knownGestures, gestureStrings, clickGesture }
